52a84ad122dd629ce1879abb2b3a3cf2d9106f77
ampersand <ampersand@underscore.space>
2016-10-21 20:44:51 +0100

    structure change. error passed back from ValidDataFolder() instead of handled in function

--- messycloud.go
+++ messycloud.go
@@ -9,22 +9,29 @@ import (
 
 func main() {
     config := jconfig.LoadConfig("./settings.json")
-    datafolder := config.GetString("data_folder")
+    datafolder := config.GetString("datafolder")
+    if datafolder == "" {
+      log.Fatalln("datafolder not specifed in settings.json")
+    }
 
-    if err := CheckDataFolder(datafolder); err == nil {
-        log.Printf("Folder %s is valid", datafolder)
+    if err := ValidDataFolder(datafolder); err == nil {
+      log.Printf("Folder %s is valid\n", datafolder)
     } else {
-        log.Fatalln("Folder is not valid:", err)
-    }   
+      log.Fatalln(err)
+    }
 }
 
-func CheckDataFolder(folderpath string) (err error) {
-    // ##TODO## also check persmissions
-    finfo, err := os.Stat(folderpath)
-    if err == nil {
-        if !finfo.IsDir() {
-            err = fmt.Errorf("Specified path is not a folder")
-        }
+func ValidDataFolder(folderpath string) (error) {    
+    // Does folderpath exist?
+    if finfo, err := os.Stat(folderpath); err == nil {
+      // Is folderpath a folder?
+      if finfo.IsDir() {
+        // ##TODO## - Do I have required permissions to folderpath?
+        return nil
+      } else {
+        return fmt.Errorf("%s: not a folder", folderpath)
+      }
+    } else {
+      return err
     }
-    return
 }
---
+++ watchfolders.go
@@ -0,0 +1,5 @@
+package watchfolder
+
+func main() {
+
+}
